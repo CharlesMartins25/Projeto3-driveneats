6d655ceb1b2442e76337d333c53b3fe1e3dc79d3
Charles <charles.marttins@gmail.com>
2022-04-03 19:25:20 -0300

    inicio java

---
+++ scripts.js
@@ -0,0 +1,53 @@
+function primeiroprato() {
+document.getElementById("Comida").style.borderColor = "green"
+document.getElementById("Comida2").style.borderColor = "white"
+document.getElementById("Comida3").style.borderColor = "white"
+}
+function segundoprato(){
+    document.getElementById("Comida2").style.borderColor = "green"
+document.getElementById("Comida").style.borderColor = "white"
+document.getElementById("Comida3").style.borderColor = "white"
+  
+}
+function terceiroprato(){
+    document.getElementById("Comida3").style.borderColor = "green"
+document.getElementById("Comida2").style.borderColor = "white"
+document.getElementById("Comida").style.borderColor = "white"
+  
+}
+function primeirabebida(){
+    document.getElementById("bebida1").style.borderColor = "green"
+document.getElementById("bebida2").style.borderColor = "white"
+document.getElementById("bebida3").style.borderColor = "white"
+  
+}
+function segundabebida(){
+    document.getElementById("bebida2").style.borderColor = "green"
+document.getElementById("bebida1").style.borderColor = "white"
+document.getElementById("bebida3").style.borderColor = "white"
+  
+}
+function terceirabebida(){
+    document.getElementById("bebida3").style.borderColor = "green"
+document.getElementById("bebida2").style.borderColor = "white"
+document.getElementById("bebida").style.borderColor = "white"
+  
+}
+function primeirasobremesa(){
+    document.getElementById("sobremesa").style.borderColor = "green"
+document.getElementById("sobremesa2").style.borderColor = "white"
+document.getElementById("sobremesa3").style.borderColor = "white"
+  
+}
+function segundasobremesa(){
+    document.getElementById("sobremesa2").style.borderColor = "green"
+document.getElementById("sobremesa").style.borderColor = "white"
+document.getElementById("sobremesa3").style.borderColor = "white"
+  
+}
+function terceirasobremesa(){
+    document.getElementById("sobremesa3").style.borderColor = "green"
+document.getElementById("sobremesa2").style.borderColor = "white"
+document.getElementById("sobremesa").style.borderColor = "white"
+  
+}
